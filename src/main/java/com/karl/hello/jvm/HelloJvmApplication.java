14ce89c66d285169166d0e610e58e0d227a8fce9
DESKTOP-K61MA6V\85983 <xiezw@ruhrtec.cn>
2020-05-17 19:58:21 +0800

    hi jvm

---
+++ src/main/java/com/karl/hello/jvm/HelloJvmApplication.java
@@ -0,0 +1,13 @@
+package com.karl.hello.jvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJvmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloJvmApplication.class, args);
+    }
+
+}
